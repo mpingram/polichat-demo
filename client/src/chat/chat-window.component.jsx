0cc27f38a85e3887ce7366995cc55b09f073dae7
Michael Ingram <mpingram92@gmail.com>
2017-03-07 16:23:17 -0600

    [WIP] implement ChatWindow scroll to bottom

--- client/src/chat/chat-window.component.jsx
+++ client/src/chat/chat-window.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 import Message from "./message.component";
 import TypingFeedbackMessage  from "./typing-feedback-message.component";
@@ -8,29 +9,69 @@ import style from "./chat-window.component.scss";
 const propTypes = {
   messages: React.PropTypes.arrayOf( React.PropTypes.object ),
   partnerTyping: React.PropTypes.bool,
+  scrollToBottom: React.PropTypes.bool
 };
 
 const defaultProps = {
+  scrollToBottom: false,
   messages: [],
-  partnerTyping: true
+  partnerTyping: true,
 };
 
-const ChatWindow = ( props ) => {
+class ChatWindow extends React.Component {
+  
+  constructor( props ){
+    super( props );
+  
+    this.state = {
+      scrollAdjusted: false,
+    };
 
-  return (
-    <div className={style.window}>
-        { props.messages.map( (message, i) => {
-            return <Message 
-                      key={message.sent.valueOf()} 
-                      text={message.text} 
-                      sent={message.sent} 
-                      sender={message.sender} 
-                    />
-          })}
-      
-        { props.partnerTyping ? <TypingFeedbackMessage/> : undefined }
-    </div>
-  )
+    this.scrollToBottom = () => {
+      console.log("scroll to bottom");
+      const messageEnd = ReactDOM.findDOMNode( this.messageEnd );
+      try {
+        messageEnd.scrollIntoView({
+          behavior: "smooth",
+          block: "end"
+        });
+      } catch(e) {
+        messageEnd.scrollIntoView(false);
+      }
+    }
+
+    this.handleScroll = () => {
+      if ( this.state.scrollAdjusted === false ){
+        this.setState({
+          scrollAdjusted: true
+        });
+      }
+    }
+  }
+
+  componentWillUpdate(){
+    if ( this.props.scrollToBottom === true || this.state.scrollAdjusted === false ){
+      this.scrollToBottom();
+    }
+  }
+
+  render(){
+    return (
+      <div className={style.window} onScroll={this.handleScroll}>
+          { this.props.messages.map( (message) => {
+              return <Message 
+                        key={message.sent.valueOf()} 
+                        text={message.text} 
+                        sent={message.sent} 
+                        sender={message.sender} 
+                      />
+            })}
+        
+          { this.props.partnerTyping ? <TypingFeedbackMessage/> : undefined }
+          <div style={{position: "relative", float: "left", clear: "both"}} ref={ (el) => { this.messageEnd = el; } } />
+      </div>
+    )
+  }
 };
 
 ChatWindow.propTypes = propTypes;
